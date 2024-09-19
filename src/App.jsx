54c075dc917e1532c818481616e1a82e251ad00d
Oleksandr Pelykh <pelykholeksander@gmail.com>
2024-09-19 20:54:57 +0300

    redirect v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './style/style.css'
 
 import Upper from "./components/upper/upper";
